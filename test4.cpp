1aab80fbcc02410cd6cf2e3a90435e51a314dd36
Pushkal Kishore <kpushkal122@gmail.com>
2022-08-07 19:23:00 +0530

    made some changes

---
+++ test4.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+#define rep(i,a,b) for (int i=a; i<b; i++)
+
+
+int32_t main()
+{
+
+
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif
+
+
+    int t;cin>> t;
+    cin>>t;
+    while(t--)
+    {
+       cout<<"hello world";
+    }
+
+    return 0;
+}
+
+
